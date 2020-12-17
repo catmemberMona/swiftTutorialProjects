ecf6408630b19153e775ba598bf7429db9c34832
Mona Zheng <mona.a.zheng@gmail.com>
2020-12-17 16:15:24 -0500

    feat: background color and text change on click

--- tictactoeTutorial/tictactoeTutorial/ViewController.swift
+++ tictactoeTutorial/tictactoeTutorial/ViewController.swift
@@ -14,8 +14,24 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     @IBAction func buSelectEvent(_ sender: UIButton) {
-        let buSelect = sender
-        print(buSelect.tag)
+        let buSelect = sender // whichever button triggered the event
+        playGame(buSelect: buSelect)
+    }
+    
+    var ActivePlayer = 1
+    
+    func playGame(buSelect: UIButton){
+        
+        if ActivePlayer == 1 {
+            buSelect.setTitle("X", for: .normal) // text for button
+            buSelect.backgroundColor = UIColor(red: 100/255, green: 100/255, blue: 255/255, alpha: 0.8)
+            ActivePlayer = 2
+            
+        } else {
+            buSelect.setTitle("O", for: .normal)
+            buSelect.backgroundColor = UIColor(red: 52/255, green: 114/255, blue: 0/255, alpha: 0.6)
+            ActivePlayer = 1
+        }
     }
     
 
