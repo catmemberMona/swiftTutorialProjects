84c38bcf5fb270cb1267960c788723743f62535c
Mona Zheng <mona.a.zheng@gmail.com>
2020-12-17 15:08:52 -0500

    add tag to each button as identifier for action

--- tictactoeTutorial/tictactoeTutorial/ViewController.swift
+++ tictactoeTutorial/tictactoeTutorial/ViewController.swift
@@ -13,7 +13,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func buSelectEvent(_ sender: UIButton) {
+        let buSelect = sender
+        print(buSelect.tag)
+    }
+    
 
 }
 
