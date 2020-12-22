51aeba225f1c060eea52f69783b923cc64a84d42
Mona Zheng <mona.a.zheng@gmail.com>
2020-12-22 14:21:35 -0500

    feat: auto play for player 2/computer

--- tictactoeTutorial/tictactoeTutorial/ViewController.swift
+++ tictactoeTutorial/tictactoeTutorial/ViewController.swift
@@ -13,6 +13,18 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBOutlet weak var bu9: UIButton!
+    @IBOutlet weak var bu8: UIButton!
+    @IBOutlet weak var bu7: UIButton!
+    @IBOutlet weak var bu6: UIButton!
+    @IBOutlet weak var bu5: UIButton!
+    @IBOutlet weak var bu4: UIButton!
+    @IBOutlet weak var bu3: UIButton!
+    @IBOutlet weak var bu2: UIButton!
+    @IBOutlet weak var bu1: UIButton!
+    
+    
     @IBAction func buSelectEvent(_ sender: UIButton) {
         let buSelect = sender // whichever button triggered the event
         playGame(buSelect: buSelect)
@@ -27,8 +39,13 @@ class ViewController: UIViewController {
             buSelect.setTitle("X", for: .normal) // text for button
             buSelect.backgroundColor = UIColor(red: 100/255, green: 100/255, blue: 255/255, alpha: 0.8)
             player1.append(buSelect.tag)
+            print("where is the error place 1")
             findWinner(player: player1) // checks if the player won
+            print("where is the error place 2")
             ActivePlayer = 2
+            print("where is the error place 3")
+            AutoPlay()
+            print("where is the error place 4", player1, player2)
             
         } else {
             buSelect.setTitle("O", for: .normal)
@@ -81,14 +98,53 @@ class ViewController: UIViewController {
             }
             
             // freeze game & ask for rematch
-            let alert = UIAlertController(title: msg, message: "Play Again", preferredStyle: UIAlertController.Style.alert)
-            alert.addAction(UIAlertAction(title: "ok", style: UIAlertAction.Style.default, handler: nil))
+            let alert = UIAlertController(title: msg, message: "Let's have a rematch!", preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil))
             self.present(alert, animated: true, completion: nil)
         }
-
-        
     }
     
+    func AutoPlay(){
+        
+        // scan empty cells
+        var emptycells = [Int]()
+        
+        // when neither player one or two picked the cells, add to selection
+        for tag in 1...9 {
+            if !(player1.contains(tag) || player2.contains(tag)){
+                emptycells.append(tag)
+            }
+        }
+        
+        let randomTag = Int.random(in: 0..<emptycells.count)
+        let cellId = emptycells[randomTag]
+        var buSelect:UIButton?
+        
+        switch cellId {
+        case 1:
+            buSelect = bu1
+        case 2:
+            buSelect = bu2
+        case 3:
+            buSelect = bu3
+        case 4:
+            buSelect = bu4
+        case 5:
+            buSelect = bu5
+        case 6:
+            buSelect = bu6
+        case 7:
+            buSelect = bu7
+        case 8:
+            buSelect = bu8
+        case 9:
+            buSelect = bu9
+        default:
+            print("Error")
+        }
+        playGame(buSelect: buSelect!)
+        
+    }
 
 }
 
