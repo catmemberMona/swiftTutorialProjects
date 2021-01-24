00be904966e0181a73151250e449287429f2419b
Mona Zheng <mona.a.zheng@gmail.com>
2021-01-24 16:50:17 -0500

    init food app w/ the assets

---
+++ FoodApp/FoodApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodApp
+//
+//  Created by mona zheng on 1/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
