e9f422d2b12320d05452b788f0b93d3494b43b4e
Mona Zheng <mona.a.zheng@gmail.com>
2021-01-31 22:32:00 -0500

    New: created load page New: Use weather api using URL to obtain data

--- findSunrise/findSunrise/ViewController.swift
+++ findSunrise/findSunrise/ViewController.swift
@@ -8,12 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private var apiKey: String {
+          get {
+            // 1
+            guard let filePath = Bundle.main.path(forResource: "OpenWeatherMapInfo", ofType: "plist") else {
+              fatalError("Couldn't find file 'OpenWeatherMapInfo.plist'.")
+            }
+            // 2
+            let plist = NSDictionary(contentsOfFile: filePath)
+            guard let value = plist?.object(forKey: "OWM_API_KEY") as? String else {
+              fatalError("Couldn't find key 'OWM_API_KEY' in 'OpenWeatherMapInfo.plist'.")
+            }
+            return value
+          }
+        }
+    
+    @IBOutlet weak var txtLocation: UITextField!
+    @IBOutlet weak var tvShowTime: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func buLookUpTime(_ sender: Any) {
+        
+        let url = "http://api.openweathermap.org/data/2.5/weather?zip=11356,us&appid=\(apiKey)"
+        
+        // get data from url
+        do {
+            let appURL = URL(string: url)
+            let data = try Data(contentsOf: appURL! as URL)
+            let json = try JSONSerialization.jsonObject(with: data) as! [String: Any]
+            print(json)
+        } catch {
 
+            tvShowTime.text = "Cannot reach server"
+        }
+    }
+    
 }
 
