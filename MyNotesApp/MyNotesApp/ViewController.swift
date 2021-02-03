708e7436470f96a219ef8e5abad34c9e020288d2
Mona Zheng <mona.a.zheng@gmail.com>
2021-02-03 18:17:11 -0500

    create notes app and layout

---
+++ MyNotesApp/MyNotesApp/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MyNotesApp
+//
+//  Created by mona zheng on 2/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    // Outlets
+    @IBOutlet weak var txtTitle: UITextField!
+    @IBOutlet weak var txtDes: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    // Actions
+    @IBAction func buSave(_ sender: Any) {
+    }
+}
+
