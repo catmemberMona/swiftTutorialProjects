22663f236a4aa2de3542110118a9fd0ae396f27b
Mona Zheng <mona.a.zheng@gmail.com>
2021-02-13 22:00:21 -0500

    learn: save name to database

--- storeApp/storeApp/ViewController.swift
+++ storeApp/storeApp/ViewController.swift
@@ -6,14 +6,30 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtInputStoreName: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buSave(_ sender: Any) {
+        let newStore = StoreType(context: context)
+        newStore.name = txtInputStoreName.text
+        do {
+            ad.saveContext()
+            txtInputStoreName.text = ""
+        } catch {
+            print("Did not save store name")
+        }
+    }
+    
+    @IBAction func buBack(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
 }
 
